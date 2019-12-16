543507e1626e6e5a8732c1ab8bb381a8ea4e916f
Koenraad Van den Eeckhout <koenraad@voltza.be>
2019-12-16 15:19:25 +0100

    Complete refactoring and bugfixes of the compose email and yml handling parts

--- make_invoice.py
+++ make_invoice.py
@@ -2,98 +2,68 @@ import argparse
 import os
 import yaml
 import companyspecific
+import datetime
 
 BASEPATH = companyspecific.BASEPATH
 TEMPLATEPATH = companyspecific.TEMPLATEPATH
 
-parser = argparse.ArgumentParser(description='Invoice generation script')
-parser.add_argument('--month', '-m', action="store", type=str,
-                   help='month of the invoices to be generated, eg:  201910')
+def findnextinvoicenr(year, month):
+    # check the latest invoice counter
+    # first check in the current folder for existing yml files
+    latest = 0
+    while (latest == 0):
+        path = BASEPATH + str(year) + '/' + str(month) + "/outgoing/"
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if ".yml" in file:
+                    if (file[:10].isdigit()):
+                        if int(file[:10])>latest:
+                            latest=int(file[:10])
+        month = month - 1
+        #Exception for start of new year
+        if month == 0:
+            break
+    print "Last used invoice number: " + str(latest)
+    if latest == 0:
+        next = year*1000000
+    else:
+        next = latest + 1
+    return next
 
-args = parser.parse_args()
-year = args.month[0:4]
-month = args.month[4:]
-
-## Generate yml files
-# check the latest invoice counter
-# first check in the current folder for existing yml files
-latest = 0
-checking_month = int(month)
-while (latest == 0):
-    path = BASEPATH + year + '/' + str(checking_month) + "/outgoing/"
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if ".yml" in file:
-                if (file[:10].isdigit()):
-                    if int(file[:10])>latest:
-                        latest=int(file[:10])
-    checking_month = checking_month - 1
-    #Exception for start of new year
-    if checking_month == 0:
-        break
-print "Last used invoice number: " + str(latest)
-if latest == 0:
-    next = int(year)*1000000
-else:
-    next = latest + 1
-
-# find all active customers
-customerlist = companyspecific.customerlist
-exceptionlist = companyspecific.exceptionlist
-
-#for customer in customerlist:
-    # find billing information for the customer
-    # get the invoice data
-    # write the yml file
-
-
-#TODO '---' at the end needs to be removed for python but added for pandoc
-# increment the invoice counter and append to the filename
-for customer in customerlist:
-    if customer in exceptionlist:
-        continue
-    path = BASEPATH + year + '/' + month + "/outgoing/" + customer + '/'
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if ".yml" in file:
-                with open(path+file) as f:
-                     list_doc = yaml.load(f)
-                #TODO doesn't seem to work
-                list_doc["invoice-nr"] = str(next)
-                print "added invoice-nr (" + str(next) + ") to: " + file
-                with open(file, "w") as f:
-                    yaml.dump(list_doc, f)
-                os.system("mv " + path + file + " " + path + str(next) + '-' + file)
-                next = next + 1
-
-## Generate pdf timesheets to go with the invoices
-
-## Generate pdf invoice based on yml files
-for customer in customerlist:
-    if customer in exceptionlist:
-        continue
-    path = BASEPATH + year + '/' + month + "/outgoing/" + customer + '/'
-    file = makepdf(path)
-    if not(file == ""):
-        ## Compose the email
-        composeEmail(file)
-
-def makepdf(path):
-    os.system("cp " + path + "*.yml " + TEMPLATEPATH)
-    os.system("make")
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if ".yml" in file:
-                #print file
-                os.system("cp " + TEMPLATEPATH + "*.pdf " + path + file[:-3] + "pdf")
-                #cleanup
-                os.system("rm " + TEMPLATEPATH + "*.yml")
-                os.system("rm " + TEMPLATEPATH + "*.pdf")
-                break
-        else:
-            print "yml filename not found"
-            return ""
+def updateinvoicenr(file):
+    now = datetime.datetime.now()
+    next = findnextinvoicenumber(now.year, now.month)
+    with open(file) as f:
+         list_doc = yaml.load(f)
+    #TODO doesn't seem to work
+    list_doc["invoice-nr"] = str(next)
+    print "added invoice-nr (" + str(next) + ") to: " + file
+    with open(file, "w") as f:
+        f.write("---\n")
+        yaml.dump(list_doc, f)
+        f.write("...")
+    filename = file.split('/')[-1]
+    path = file[:-len(filename)]
+    if len(filename.split('-'))>1 and filename.split('-')[0].isdigit():
+        file = path + str(next) + '-' + filename.split('-')[1]
+    else:
+        file = path + str(next) + '-' + filename
+    os.system("mv " + path + filename + " " + file)
     return file
+
+def makeinvoice(file):
+    file = updateinvoicenr(file)
+    makepdf(file)
+    composeEmail(file)
+
+def makepdf(file):
+    os.system("cp " + file + " " + TEMPLATEPATH)
+    os.system("make")
+    #print file
+    os.system("cp " + TEMPLATEPATH + "*.pdf " + file[:-3] + "pdf")
+    #cleanup
+    os.system("rm " + TEMPLATEPATH + "*.yml")
+    os.system("rm " + TEMPLATEPATH + "*.pdf")
 
 def composeEmail(file):
     with open(file) as f:
@@ -102,26 +72,45 @@ def composeEmail(file):
         "to": list_doc["to-email"],
         "cc": list_doc["cc-email"],
         "bcc":companyspecific.bccemail,
-        "subject": "Invoice " + list_doc["invoice-nr"]
+        "subject": "Invoice " + str(list_doc["invoice-nr"])
         }
-    #email["attachment"] = "" #TODO add attachments
-    email["body"] = """Dear Customer,
-
-        Please find your invoice attached to this e-mail.
-        Further detail regarding the invoice items can be found in """
-    if  (list_doc["timetracking"]=="True"):
-        email["body"] = email["body"] + "your timetracking system."
+    email["attachment"] = file[:-3] + "pdf"
+    #TODO more attachments
+    if  (list_doc["timetracking"]==True):
+        email["message"] = companyspecific.message_timetracking
     else:
-        email["body"] = email["body"] + "the accompanying ZIP file."
-    email["body"] = email["body"] + """
+        email["message"] = companyspecific.message_notimetracking
+    os.system("thunderbird -compose \"" + ",".join(['%s=%s' % (key, value) for (key, value) in email.items()])+'\"')
 
-        The invoice is generated in PDF format and replaces the paper copy.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Invoice generation script')
+    parser.add_argument('--month', '-m', action="store", type=str,
+                       help='month of the invoices to be generated, eg:  201910')
+    parser.add_argument('--single', '-s', action="store", default="", type=str,
+                       help='filename of single invoice to be generated, eg:  /home/user/test.yml')
+    args = parser.parse_args()
+    if not (args.single==""):
+        makeinvoice(args.single)
+    year = args.month[0:4]
+    month = args.month[4:]
 
-        Payment can be made to Voltza by bank transfer to the bank specified at the bottom of your invoice. Please include your invoice number as the transaction reference.
+    ## Generate yml files
+    # find all active customers
+    customerlist = companyspecific.customerlist
+    exceptionlist = companyspecific.exceptionlist
 
-        For any questions concerning this invoice: contact us at invoices@voltza.be
+    #for customer in customerlist:
+        # find billing information for the customer
+        # get the invoice data
+        # write the yml file
 
-        Yours sincerely,
-
-        Koenraad Van den Eeckhout"""
-    os.system("thunderbird -compose " + ",".join(['%s=%s' % (key, value) for (key, value) in email.items()]))
+    # increment the invoice counter and append to the filename
+    for customer in customerlist:
+        if customer in exceptionlist:
+            continue
+        path = BASEPATH + year + '/' + month + "/outgoing/" + customer + '/'
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if ".yml" in file:
+                    ## Generate pdf timesheets to go with the invoices
+                    makeinvoice(path + file)
